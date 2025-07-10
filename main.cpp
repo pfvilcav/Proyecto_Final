964cc717aad1d3fcae2e0808e0c2cf857c2d46a6
pfvilcav <pfvilcav@unjbg.edu.pe>
2025-07-10 01:31:07 -0500

    Implementación de mas información para los productos a ingresar y otros arreglos.
    Proximamente los productos introducidos se guardaran en un '*.txt' por lo tanto se fue introduciendo código que sera útil a futuro.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
-#include <cstdint> 
-#include "termcolor.hpp"
+#include<cstdint> // para que termcolor.hpp funcione
+#include<ctime>
+#include<fstream>
+#include"termcolor.hpp"
 
 using namespace std;
 using namespace termcolor;
 
+string fecha_hora() { //funcion string para hallar fecha y hora actual
+    time_t t = time(nullptr);
+    char buffer[30];
+    strftime(buffer, sizeof(buffer), "%d/%m/%Y %H:%M:%S", localtime(&t));
+    return buffer;
+}
+
 struct info{
-    string nomb;
-    int precio, cant;
+    string nomb, fecha, t_alimento, marca, autor, genero;
+    double precio;
+    int cant;
 } PRODUCTOS[250];
 
 int menu() { //menu (modularizar)
@@ -18,13 +28,14 @@ int menu() { //menu (modularizar)
     cout<<on_green<<"//////////////////////////////////////////////////////////"<<reset<<endl;
     cout<<on_blue<<"1) Agregar producto."<<endl<<"2) Editar producto."<<endl<<"3) Eliminar producto."<<endl;
     cout<<"4) Buscar producto por nombre."<<endl<<"5) Ver lista de productos."<<endl<<"6) Ver resumen del inventario"<<endl;
-    cout<<"7) Filtrar productos por categoria o proveedor."<<endl<<"8) Guardar productos."<<endl<<"9) Salir."<<reset<<endl;
+    cout<<"7) Filtrar productos por categoria o proveedor."<<endl<<"8) Salir."<<reset<<endl;
     cout<<on_yellow<<"Ingrese la opcion que usted desee realizar:"<<reset<<endl;
     cin>>opcion;
     return opcion;
 }
 
 int main() {
+    char rpta;
     int n, catAa=0, catBb=0, catCc=0, catDd=0;
     int A, B, C, D; //A: PAPELERIA // B: ELECTRONICOS // C:ALIMENTOS // D: LIBROS 
     string catA[50], catB[50], catC[50], catD[50]; // categorias
@@ -32,44 +43,61 @@ int main() {
         n=menu();
         system("cls");
         switch (n) {
-            case 1: //agregar
+            case 1: //////agregar
             int k;
             char cat;
             cout<<green<<"*Cuantos tipos de productos quieres ingresar?"<<reset<<endl; cin>>k;
             cout<<endl<<on_yellow<<"Ingrese los productos:"<<reset<<endl;
             for (int i=0; i<k; i++) { // luego para almacenar en .txt se debe crear variable que almacene el total.
-                cin.ignore();
-                cout<<green<<"Nombre del producto:"; getline(cin,PRODUCTOS[i].nomb);
+                PRODUCTOS[i].fecha=fecha_hora(); //halla la fecha y hora exacta
                 cout<<"\nCategoria: "<<red<<"Elija entre: A) PAPELERIA | B) ELECTRONICOS | C) ALIMENTOS | D) LIBROS"<<endl; cin>>cat;
-                switch(cat) {
+                cin.ignore();
+                switch(cat) { //categorias
                     case 'A': case 'a':
+                        cout<<green<<"Nombre del producto:"; getline(cin,PRODUCTOS[i].nomb);
                         catA[i]=PRODUCTOS[i].nomb;
                         catAa++;
                     break;
 
                     case 'B': case 'b':
+                        cout<<green<<"Nombre del producto:"; getline(cin,PRODUCTOS[i].nomb);
+                        cout<<green<<"\nMarca del producto: "; cin>>PRODUCTOS[i].marca;
                         catB[i]=PRODUCTOS[i].nomb;
                         catBb++;
                     break;
 
                     case 'C': case 'c':
+                        cout<<green<<"Nombre del producto:"; getline(cin,PRODUCTOS[i].nomb);
+                        cout<<green<<"\nTipo de alimento: "; cin>>PRODUCTOS[i].t_alimento;
                         catC[i]=PRODUCTOS[i].nomb;
                         catCc++;
                     break;
 
                     case 'D': case 'd':
+                        cout<<green<<"Nombre del producto:"; getline(cin,PRODUCTOS[i].nomb);
+                        cout<<green<<"\nAutor: "; getline(cin,PRODUCTOS[i].autor);
+                        cout<<"\nGenero: "; cin>>PRODUCTOS[i].genero;
                         catD[i]=PRODUCTOS[i].nomb;
                         catDd++;
                     break;
                 }
+                //cout<<green<<"Nombre del producto:"; getline(cin,PRODUCTOS[i].nomb); //nombre
                 cout<<green<<"\nPrecio:"; cin>>PRODUCTOS[i].precio;
                 cout<<"\nCantidad:"; cin>>PRODUCTOS[i].cant;
-                if (i==k-1) {
+                cout<<"\nFecha de ingreso: "<<PRODUCTOS[i].fecha;
+                if (i==k-1) { //termina el case A en funcion de k=cantidad de productos, si no, lo resetea
                     cout<<reset<<on_blue<<"\nHecho!";
                     system("pause");
+                    cout<<on_green<<"Desea guardar los productos que acaba de ingresar? (S/N): "; cin>>rpta;
+                    if (rpta=='S' || rpta=='s') {
+                        // falta
+                    }
+                    else {
+                        // falta
+                    }
                 }
                 else {
-                    cout<<reset<<on_yellow<<"==================================="<<reset<<endl;
+                    cout<<reset<<on_yellow<<"\n==================================="<<reset<<endl;
                 }
 
             }
@@ -122,15 +150,16 @@ int main() {
             } 
 
             break;
-            case 8: // guardar productos
-
+            case 8: // cerrar
+                return 0;
             break;
+
             default:
             cout<<on_red<<"Opcion no valida..."<<reset<<endl;
                 system("pause");
                 break;
         }
     }
-    while (n!=9); //salir
+    while (n!=8); //salir
     return 0;
 }
